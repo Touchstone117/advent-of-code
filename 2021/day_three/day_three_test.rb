77d59e00168c7903aab84c299853d9a6b4f05993
Daniel Hunt <dhunt@amplience.com>
2021-12-04 16:30:45 +0000

    chore: day three part two

--- 2021/day_three/day_three_test.rb
+++ 2021/day_three/day_three_test.rb
@@ -14,23 +14,23 @@ end
 
 class DayThreeTest < MiniTest::Test
   def test_day_one_part_one_practice
-    assert_equal 198, DayThreePartOne.new(DayThreeInput.practice).solve_part_one
+    assert_equal 198, DayThree.new(DayThreeInput.practice).solve_part_one
   end
 
   def test_day_one_part_one_real
-    assert_equal 2_035_764, DayThreePartOne.new(DayThreeInput.real).solve_part_one
+    assert_equal 2_035_764, DayThree.new(DayThreeInput.real).solve_part_one
   end
 
-  # def test_day_one_part_two_practice
-  #   assert_equal 900, DayThreePartTwo.new(DayThreeInput.practice).solve_part_two
-  # end
+  def test_day_one_part_two_practice
+    assert_equal 230, DayThree.new(DayThreeInput.practice).solve_part_two
+  end
 
-  # def test_day_one_part_two_real
-  #   assert_equal 2_006_917_119, DayThreePartTwo.new(DayThreeInput.real).solve_part_two
-  # end
+  def test_day_one_part_two_real
+    assert_equal 2_817_661, DayThree.new(DayThreeInput.real).solve_part_two
+  end
 end
 
-class DayThreePartOne
+class DayThree
   def initialize(input)
     @input = input
     @horizontal = 0
@@ -61,10 +61,28 @@ class DayThreePartOne
     raise 'neither'
   end
 
-  def index_bits(index)
+  def oxygen_generator_rating_calc(input)
+    # most common, equal = 1
+    return '0' if input[:ones] < input[:zeros]
+    return '1' if input[:ones] > input[:zeros]
+    return '1' if input[:ones] == input[:zeros]
+
+    raise 'none'
+  end
+
+  def co2_scrubber_rating_calc(input)
+    # least common, equal = 0
+    return '0' if input[:ones] > input[:zeros]
+    return '1' if input[:ones] < input[:zeros]
+    return '0' if input[:ones] == input[:zeros]
+
+    raise 'neither'
+  end
+
+  def index_bits(index, array)
     index_bits = []
 
-    @input.each do |line|
+    array.each do |line|
       index_bits << line[index].to_i
     end
 
@@ -76,12 +94,45 @@ class DayThreePartOne
     epsilon_rate = ''
 
     @input[0].strip.length.times do |i|
-      counted = count_bits(index_bits(i))
+      counted = count_bits(index_bits(i, @input))
 
       gamma_rate += gamma_rate_calc(counted)
       epsilon_rate += epsilon_rate_calc(counted)
     end
 
     gamma_rate.to_i(2) * epsilon_rate.to_i(2)
+  end
+
+  def delete_from_index(array, choice, index)
+    array.delete_if do |line|
+      line[index] != choice
+    end
+  end
+
+  def solve_part_two
+    oxygen_rate = @input.dup
+
+    @input[0].strip.length.times do |i|
+      counted = count_bits(index_bits(i, oxygen_rate))
+
+      oxygen_rate_choice = oxygen_generator_rating_calc(counted)
+
+      oxygen_rate = delete_from_index(oxygen_rate, oxygen_rate_choice, i)
+
+      break if oxygen_rate.length == 1
+    end
+
+    co2_rate = @input.dup
+    @input[0].strip.length.times do |i|
+      counted = count_bits(index_bits(i, co2_rate))
+
+      co2_rate_choice = co2_scrubber_rating_calc(counted)
+
+      co2_rate = delete_from_index(co2_rate, co2_rate_choice, i)
+
+      break if co2_rate.length == 1
+    end
+
+    oxygen_rate.first.to_i(2) * co2_rate.first.to_i(2)
   end
 end
